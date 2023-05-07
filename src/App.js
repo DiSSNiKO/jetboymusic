7eccc33a555964d08e6751660a0bfdb549112f6d
DiSSNiKO <nikokalanda@gmail.com>
2023-05-07 09:52:27 +0400

    Blueee mooooon

--- src/App.js
+++ src/App.js
@@ -7,26 +7,47 @@ import SongChoice from './components/SongChoice';
 function App() {
   const [songs, setSongs] = useState([
     {
-      path: "/musicWry/DADDYDADDYDOO.mp3",
-      name: "Daddy Daddy Doo",
+      path: "/musicWry/Ain't No Rest For The Wicked.mp3",
+      name: "Ain't No Rest For The Wicked",
     },
     {
-      path: "/musicWry/pop-stars.mp3",
-      name: "Pop Stars",
+      path: "/musicWry/Blue Moon.mp3",
+      name: "Blue Moon",
     },
     {
-      path: "/musicWry/racing-into-the-night.mp3",
-      name: "Racing Into The Night",
+      path: "/musicWry/Enter Sandman.mp3",
+      name: "Enter Sandman",
     },
     {
-      path: "/musicWry/renaicirculation.mp3",
-      name: "Renai Circulation",
+      path: "/musicWry/TUSK.mp3",
+      name: "TUSK",
     },
     {
-      path: "/musicWry/smoke-on-the-water.mp3",
+      path: "/musicWry/Smoke On The Water.mp3",
       name: "Smoke On The Water"
+    },
+    {
+      path: "/musicWry/Fly Me To The Moon.mp3",
+      name: "Fly Me To The Moon"
+    },
+    {
+      path: "/musicWry/The Super Gore Nest.mp3",
+      name: "The Super Gore Nest"
+    },
+    {
+      path: "/musicWry/Samidare 20 Love.mp3",
+      name: "Samidare 20 Love"
+    },
+    {
+      path: "/musicWry/Short Change Hero.mp3",
+      name: "Short Change Hero"
+    },
+    {
+      path: "/musicWry/The Wanderer.mp3",
+      name: "The Wanderer"
     }
   ]);
+  const [searchKey, setSearchKey] = useState('');
   const lastSongIndex = songs.length - 1;
   const [isFirst, setIsFisrt] = useState(true);
   const [currentSong, setCurrentSong] = useState(0);
@@ -62,10 +83,14 @@ function App() {
         <source src="/musicWry/DADDYDADDYDOO.mp3" type="audio/mpeg" />
       </audio>
       <div className='song-list-search-cont'>
-        <SearchBar songs={songs} />
+        <SearchBar songs={songs} setSearchKey={setSearchKey} />
         <div className='songs-cont'>
           {Object.entries(songs).map((owo, index) => {
-            return <SongChoice songs={songs} currentSong={currentSong} setCurrentSong={setCurrentSong} index={index} />;
+            if (searchKey === '') {
+              return <SongChoice songs={songs} currentSong={currentSong} setCurrentSong={setCurrentSong} index={index} />;
+            } else if (songs[index]['name'].toLowerCase().includes(searchKey.toLowerCase())) {
+              return <SongChoice songs={songs} currentSong={currentSong} setCurrentSong={setCurrentSong} index={index} />;
+            }
           })}
         </div>
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
-function SearchBar() {
+import { useRef } from "react";
+
+function SearchBar(props) {
+    const { setSearchKey } = props;
+    const serch = useRef(null);
     return (
         <div className="search-bar-cont">
             <div className='magni-ikonka'>
                 <img src="/images/musnote.svg" alt="" />
             </div>
             <div className="input-n-dec">
-                <input type="text" />
+                <form action="" onSubmit={(e) => {
+                    e.preventDefault();
+                    setSearchKey(serch.current.value);
+                    console.log(serch.current.value)
+                }}>
+                    <input type="text" ref={serch} />
+                </form>
                 <div className="hover-decor"></div>
             </div>
         </div>
